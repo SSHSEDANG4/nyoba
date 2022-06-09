724a12d43af0ecfb47edebea4c59787383ee9691
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2022-06-09 22:56:52 +0700

    Update xrayssn.sh

--- xrayssn.sh
+++ xrayssn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Auth: happylife
+# Auth: happylife YES
 # Desc: xray installation script
 # 	    ws+vless,ws+trojan,ws+socks,ws+shadowsocks
 #	      grpc+vless,grpc+trojan,grpc+socks,grpc+shadowsocks
